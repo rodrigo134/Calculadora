f32003ef9ac27fc63590fee173371229ae915c97
Rodrigo Rodrigues <35582836+rodrigo134@users.noreply.github.com>
2024-02-09 22:43:17 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,16 +1,19 @@
 while True:
+    #loop will be true until a character N is inserted
     entrada = input("DESEJA SAIR [S] ou [N]")
     sair = entrada.upper().startswith('S')
     if sair:
         break
+      
     try:
         numero1=input("INSIRA O NUMERO PARA A OPERAÇÃO ")
         numero2=input("INSIRA O NUMERO PARA A OPERAÇÃO ")
+        #If the casting fails, the except will be activated
         numero1=float(numero1)
         numero2=float(numero2)
-
+        
         operador = input("ESCOLHA O OPERADOR + - * / ")
-        print()
+    #condition for choosing the operator
         if operador == '+':
             print(f'{numero1} {operador} {numero2} = {numero1 + numero2}')
 
