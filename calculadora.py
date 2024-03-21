ee433fdbf59af0b10e94fe5a0721bcca596b5090
Rodrigo Rodrigues <35582836+rodrigo134@users.noreply.github.com>
2024-03-21 14:39:18 -0300

    calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,18 +1,18 @@
 while True:
     #loop will be true until a character N is inserted
-    entrada = input("DESEJA SAIR [S] ou [N]")
-    sair = entrada.upper().startswith('S')
+    entrada = input("DO YOU WANT TO LEAVE?[Y] ou [N]")
+    sair = entrada.upper().startswith('Y')
     if sair:
         break
       
     try:
-        numero1=input("INSIRA O NUMERO PARA A OPERAÇÃO ")
-        numero2=input("INSIRA O NUMERO PARA A OPERAÇÃO ")
+        numero1=input("ENTER THE NUMBER FOR THE OPERATION :")
+        numero2=input("ENTER THE NUMBER FOR THE OPERATION :")
         #If the casting fails, the except will be activated
         numero1=float(numero1)
         numero2=float(numero2)
         
-        operador = input("ESCOLHA O OPERADOR + - * / ")
+        operador = input("CHOOSE THE OPERATOR: + , - , * , / \n")
     #condition for choosing the operator
         if operador == '+':
             print(f'{numero1} {operador} {numero2} = {numero1 + numero2}')
@@ -24,16 +24,20 @@ while True:
             print(f'{numero1} {operador}  {numero2} = {numero1 * numero2}')
 
         elif operador == '/':
+            if numero2 ==0:
+                print("DIVISION ERROR WITH ZERO \n") 
             print(f'{numero1} {operador} {numero2} = {numero1 / numero2}')
-
         else:
-            print("OPERADOR INVÁLIDO")
+            print("INVALID OPERATOR")
 
 
     except:
-        print('INSIRA NUMEROS VÁLIDOS')
+        print('ENTER VALID NUMBERS')
 
-print('TCHAU')
+print('BYEE')
+
+
+
 
 
 
